0489e9c6190e89314abedb76e50b9ef10d636527
Guoning-Chen <cgn1874@163.com>
2020-12-06 12:41:05 +0800

    修改client

--- UDP/UDPClient.py
+++ UDP/UDPClient.py
@@ -1,9 +1,9 @@
 from socket import *
-serverName = 'hostname'
+serverName = '10.150.4.248'
 serverPort = 12000
 clientSocket = socket(AF_INET, SOCK_DGRAM)
 message = input('Input lowercase sentence:')
 clientSocket.sendto(message.encode(), (serverName, serverPort))
 modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
-print(modifiedMessage.decode())
+print('从服务器主机收到信息：', modifiedMessage.decode())
 clientSocket.close()
