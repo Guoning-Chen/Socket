9406335b6e2ca7df5f1de1ed0406402ac1aec4c0
Guoning-Chen <cgn1874@163.com>
2021-01-01 21:36:28 +0800

    1

--- 02Ping/optional2/HeartbeatClient.py
+++ 02Ping/optional2/HeartbeatClient.py
@@ -1,12 +1,12 @@
 #可选练习一较简单，故略过
 from socket import *
 import time
-servername='127.0.0.1'
-serverPort=800
-clientSocket=socket(AF_INET,SOCK_DGRAM)
-for i in range(1,11):
-    stime=time.time()
-    message=str(i)+' '+str(time.time())
-    clientSocket.sendto(message.encode(),(servername,serverPort))
 
+serverName = '10.150.4.248'
+serverPort = 800
+clientSocket=socket(AF_INET,SOCK_DGRAM)
+for i in range(10):
+    send_time = time.time()
+    message = str(i + 1) + ' ' + str(time.time())
+    clientSocket.sendto(message.encode(), (serverName, serverPort))
 clientSocket.close()
--- 02Ping/optional2/HeartbeatServer.py
+++ 02Ping/optional2/HeartbeatServer.py
@@ -3,6 +3,7 @@ import random
 import time
 
 serverSocket = socket(AF_INET, SOCK_DGRAM)
+serverSocket.settimeout(0.1)
 serverPort = 800
 serverSocket.bind(('', serverPort))
 
@@ -10,19 +11,17 @@ start_time = float(time.time())
 end_time = start_time
 
 while True:
-
     try:
-        serverSocket.settimeout(0.1)
         message, address = serverSocket.recvfrom(1024)
         message = message.decode()
-        rtime = float(message.split()[1])
-        end_time = rtime
-        Ping = float(time.time()) - rtime
+        receive_time = float(message.split()[1])  # 读取信息中的时间戳
+        end_time = receive_time
+        Ping = float(time.time()) - receive_time
         print(str(message.split()[0]) + ':', Ping)
     except Exception as e:
-        if end_time == start_time:
+        if end_time == start_time:  # 还没有收到新信息，继续循环
             continue
-        if time.time() - end_time >= 1.0:
+        if time.time() - end_time >= 1.0:  # 超过1s没有收到新的信息
             print('Heartbeat pause')
             break
         else:
