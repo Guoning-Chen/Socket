a950c40ad06076bfe17d270b78be91f01744f4a9
Guoning-Chen <cgn1874@163.com>
2021-01-01 22:35:57 +0800

    1

--- 02Ping/optional2/HeartbeatServer.py
+++ 02Ping/optional2/HeartbeatServer.py
@@ -1,5 +1,4 @@
 from socket import *
-import random
 import time
 
 serverSocket = socket(AF_INET, SOCK_DGRAM)
@@ -8,20 +7,20 @@ serverPort = 800
 serverSocket.bind(('', serverPort))
 
 start_time = float(time.time())
-last_stamp = start_time
+end_time = start_time
 
 while True:
     try:
         message, address = serverSocket.recvfrom(1024)
         message = message.decode()
-        send_time = float(message.split()[1])  # 读取信息中的时间戳
-        last_stamp = send_time
-        Ping = float(time.time()) - send_time
+        receive_time = float(message.split()[1])  # 读取信息中的时间戳
+        end_time = receive_time
+        Ping = float(time.time()) - receive_time
         print(str(message.split()[0]) + ':', Ping)
     except Exception as e:
-        if last_stamp == start_time:  # 还没有收到新信息，继续循环
+        if end_time == start_time:  # 还没有收到新信息，继续循环
             continue
-        if (time.time() - last_stamp) >= 2.0:  # 超过2s没有收到新的信息
+        if time.time() - end_time >= 1.0:  # 超过1s没有收到新的信息
             print('Heartbeat pause')
             break
         else:
